b5da05c5c7a30f1fde4e5d05ae960575dca2b8d8
viniciusgasparin <vinicius.gasparin@hotmail.com>
2019-05-02 21:22:32 -0300

    viewModel e liveData

---
+++ app/src/main/java/br/com/fiap/placarapp/PlacarViewModel.kt
@@ -0,0 +1,23 @@
+package br.com.fiap.placarapp
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class PlacarViewModel: ViewModel() {
+
+    var goalHome: MutableLiveData<Int> = MutableLiveData()
+    var goalAway: MutableLiveData<Int> = MutableLiveData()
+
+    init {
+        goalAway.value = 0
+        goalHome.value = 0
+    }
+
+    fun goalAway() {
+        goalAway.value = goalAway.value?.plus(+1)
+    }
+
+    fun goalHome() {
+        goalHome.value = goalHome.value?.plus(+1)
+    }
+}
